931601c623f8a3488f3f6b1f4433bd6612387678
DouglasFaustino <107653098+DouglasFaustino@users.noreply.github.com>
2024-10-05 00:13:32 -0300

    Update script.js

--- script.js
+++ script.js
@@ -10,5 +10,6 @@ let index = 0;
 
 document.getElementById('meuBotao').addEventListener('click', function() {
     document.getElementById('texto').innerText = mensagens[index]; // Muda o texto
-    index = (index + 1) % mensagens.length; // Avança para a próxima mensagem, voltando ao início se necessário
+    index = (index + 1) % mensagens.length; // Avança para a próxima mensagem
 });
+
